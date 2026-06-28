a058ffc19fdcca165db879baddfdd03d1eeec6c7
ArtMagedon <ap20041026@yandex.ru>
2026-06-28 19:32:06 +0300

    added basic functioanl content

--- request_block.py
+++ request_block.py
@@ -1,22 +1,24 @@
 from PyQt5.QtWidgets import (
-    QWidget,
     QVBoxLayout,
     QHBoxLayout,
     QComboBox,
     QLineEdit,
     QPushButton,
     QTextEdit,
-    QGroupBox
+    QGroupBox,
+    QLabel
 )
+
+from PyQt5.QtCore import Qt
+
+import requests
 
 
 class RequestBlock(QGroupBox):
-    def __init__(self, name:str = "BLOCK", parent=None):
+    def __init__(self, data: dict, parent=None):
         super().__init__(parent)
-        self.box_Height=200
-
-
-        self.setTitle(name)
+        self.box_Height = 200
+        print(data)
         self.setMaximumHeight(260)  # Ограничение высоты
 
         # ---------- Верхняя строка ----------
@@ -27,38 +29,106 @@ class RequestBlock(QGroupBox):
             "PUT",
             "DELETE"
         ])
-        self._update_combo_style()
 
         self.url_line = QLineEdit()
         self.url_line.setPlaceholderText("url")
 
         self.send_btn = QPushButton("Send")
+        self.delete_btn = QPushButton("✕")
+        self.delete_btn.setMaximumWidth(30)
+        self.delete_btn.setObjectName("DeleteButton")
+        self.setStyleSheet("""
+            #DeleteButton {
+                background-color: #F44336;
+                color: white;
+                border-radius: 5px;
+                border: none;
+                padding: 2px 4px;
+            }
+            #DeleteButton:hover { background-color: #D32F2F; }
+        """)
 
-        top_layout = QHBoxLayout()
-        top_layout.addWidget(self.req_type_selector)
-        top_layout.addWidget(self.url_line, 1)
-        top_layout.addWidget(self.send_btn)
+        self.top_layout = QHBoxLayout()
+        self.top_layout.addWidget(self.req_type_selector)
+        self.top_layout.addWidget(self.url_line, 1)
+        self.top_layout.addWidget(self.send_btn)
+        self.top_layout.addWidget(self.delete_btn)
+
+        # ---------- Средняя строка ----------
+        self.mid_layout = QHBoxLayout()
+        self.status_code = QLabel()
+        self.mid_layout.addStretch(1) 
+        self.mid_layout.addWidget(self.status_code, 1)
+        self.mid_layout.setContentsMargins(6, 0, 0, 0) 
+        self._hide_status()
 
         # ---------- Нижняя строка ----------
         self.body_box = QTextEdit()
         self.body_box.setPlaceholderText("Body")
-        self.body_box.setFixedHeight(self.box_Height) 
-        
+        self.body_box.setFixedHeight(self.box_Height)
 
         self.response_box = QTextEdit()
         self.response_box.setPlaceholderText("Response")
-        self.response_box.setFixedHeight(self.box_Height) 
+        self.response_box.setFixedHeight(self.box_Height)
 
-        bottom_layout = QHBoxLayout()
-        bottom_layout.addWidget(self.body_box)
-        bottom_layout.addWidget(self.response_box)
+        self.bottom_layout = QHBoxLayout()
+        self.bottom_layout.addWidget(self.body_box)
+        self.bottom_layout.addWidget(self.response_box)
 
         # ---------- Основной Layout ----------
         layout = QVBoxLayout(self)
-        layout.addLayout(top_layout)
-        layout.addLayout(bottom_layout, 1)
+        layout.addLayout(self.top_layout)
+        layout.addLayout(self.mid_layout)
+        layout.addLayout(self.bottom_layout, 1)
 
-        self.req_type_selector.currentIndexChanged.connect(self._update_combo_style)
+        self.update_data(data)
+
+        self.req_type_selector.currentIndexChanged.connect(
+            self._update_combo_style)
+        self._update_combo_style()
+        self.send_btn.clicked.connect(self.sendReq)
+
+    def sendReq(self):
+        request_data = {
+            "url": self.main_url+self.url_line.text(),
+            "body": self.body_box.toPlainText(),
+            "method": self.req_type_selector.currentIndex(),
+        }
+        print(request_data)
+        try:
+            if request_data["method"] == 0:
+                resp = requests.get(request_data["url"])
+            elif request_data["method"] == 1:
+                resp = requests.post(request_data["url"], data=request_data["body"])
+            elif request_data["method"] == 2:
+                resp = requests.put(request_data["url"], data=request_data["body"])
+            elif request_data["method"] == 3:
+                resp = requests.delete(request_data["url"], data=request_data["body"])
+            else:
+                resp = None
+            if resp is not None:
+                #print(resp.text)
+                self._show_status()
+                self.response_box.setPlainText(resp.text)
+                self.status_code.setText(str(resp.status_code))
+        except Exception as e:
+            self._show_status()
+            self.response_box.setPlainText(str(e))
+            self.status_code.setText("ERROR")
+
+    def _show_status(self):
+        if self.mid_layout.indexOf(self.status_code) == -1:
+            self.mid_layout.addWidget(self.status_code, 1)
+            self.status_code.show()
+
+    def _hide_status(self):
+        self.mid_layout.removeWidget(self.status_code)
+        self.status_code.hide()
+        self.status_code.setText("")
+
+    def updateUrl(self,main_url):
+        self.main_url=main_url
+        print(self.title(), self.main_url)
 
     def _update_combo_style(self):
         index = self.req_type_selector.currentIndex()
@@ -72,13 +142,18 @@ class RequestBlock(QGroupBox):
         else:
             self.req_type_selector.setStyleSheet("")
 
-    
-    
-    def import_data(self, external_data:dict):
-        if external_data:
-            self.url=external_data["url"]
-            self.body=external_data["body"]
-            self.req_type=external_data["req_type"]
-        
+    def update_data(self, external_data: dict):
+        self.setTitle(external_data["title"])
+        self.url_line.setText(external_data["url"])
+        self.body_box.setText(external_data["body"])
+        self.req_type_selector.setCurrentIndex(external_data["method"])
 
+    def getData(self):
+        return {
+            "title": self.title(),
+            "url": self.url_line.text(),
+            "body": self.body_box.toPlainText(),
+            "method": self.req_type_selector.currentIndex(),
+        }
+    
     
--- tester.py
+++ tester.py
@@ -17,31 +17,37 @@ from PyQt5.QtCore import Qt
 from request_block import RequestBlock
 
 
-
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.default_fonts = {
-            "label": {"family": "Arial", "size": 10, "weight": "bold"},
+            "title": {"family": "Arial", "size": 10, "weight": "bold"},
+            "status_code": {"family": "Arial", "size": 10, "weight": "bold"},
             "combo": {"family": "Arial", "size": 10, "weight": "bold"},
             "entry": {"family": "Consolas", "size": 10},
             "button": {"family": "Arial", "size": 10},
             "body": {"family": "Consolas", "size": 10},
-            "output": {"family": "Consolas", "size": 10}
+            "response": {"family": "Consolas", "size": 10}
         }
-        self.file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "config_data.json")
-        self.load_data(self.file)
+        self.blocks = []
+        self.clear_module = {"title": "", "url": "", "body": "", "method": 0}
+
+        self.file = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), "config_data.json")
+        self.load_data()
 
         self.setMinimumWidth(800)
-        self.resize(800,600)
+        self.resize(800, 600)
+
+        # Контейнер для строки запроса
         self.domen_line = QLineEdit()
         self.domen_line.setPlaceholderText("Domen / IP")
+        self.domen_line.setText(self.data["config"]["main_url"])
 
-        self.send_btn = QPushButton("Save")
-
+        self.save_btn = QPushButton("Save")
         self.head_layout = QHBoxLayout()
         self.head_layout.addWidget(self.domen_line, 1)
-        self.head_layout.addWidget(self.send_btn)
+        self.head_layout.addWidget(self.save_btn)
 
         # Контейнер для блоков
         self.blocks_widget = QWidget()
@@ -55,7 +61,7 @@ class MainWindow(QWidget):
         self.block_scroll.setWidgetResizable(True)
         self.block_scroll.setFrameShape(QFrame.NoFrame)
         self.block_scroll.setWidget(self.blocks_widget)
-        
+
         self.down_layout = QHBoxLayout()
 
         # Главный Layout
@@ -63,33 +69,56 @@ class MainWindow(QWidget):
         self.layoutMain.addLayout(self.head_layout)
         self.layoutMain.addWidget(self.block_scroll)
         self.layoutMain.addLayout(self.down_layout)
-        
+
         self.btn_add_block = QPushButton("Добавить блок")
         self.down_layout.addWidget(self.btn_add_block)
         self.btn_add_block.clicked.connect(self.add_block_bt)
-        
-        for i in range(self.num_placeholder):
-            self.add_block()
+        self.save_btn.clicked.connect(self.save_data)
+        self.domen_line.textChanged.connect(self.updateUrl)
+
+        for i in range(len(self.data["data"])):
+            self.add_block(self.data["data"][i])
+        self.updateUrl()
+
+
+    def updateUrl(self):
+        for i in self.blocks:
+            i.updateUrl(self.domen_line.text())
+            print(i, self.domen_line.text())
 
     def add_block_bt(self):
-        self.num_placeholder+=1
+        self.data["data"].append(self.clear_module)
         self.add_block()
-        print(self.num_placeholder)
+        print(len(self.data["data"]))
 
-    def add_block(self):
-        block = RequestBlock(name="Block " + str(self.block_layout.count()))
+    def add_block(self, data=None):
+        if data:
+            block = RequestBlock(data)
+        else:
+            data = self.clear_module
+            data["title"] = "Block " + str(self.block_layout.count())
+            block = RequestBlock(data)
         self.block_layout.insertWidget(self.block_layout.count() - 1, block)
+        self.blocks.append(block)
 
-    def load_data(self, SAVE_FILE:str):
-        if not os.path.exists(SAVE_FILE):
+    def load_data(self):
+        if not os.path.exists(self.file):
             return
-        with open(SAVE_FILE, "r", encoding="utf-8") as f:
-            data = json.load(f)
-        self.main_url="https://"+data["config"]["main_url"]
-        self.num_placeholder=len(data["data"])
-        if self.num_placeholder==0: self.num_placeholder+=1
-        print(self.main_url)
-        print(self.num_placeholder)
+        with open(self.file, "r", encoding="utf-8") as f:
+            self.data = json.load(f)
+        if len(self.data["data"]) == 0:
+            self.data["data"].append(self.clear_module)
+        print(self.data["config"]["main_url"])
+        print(len(self.data["data"]))
+
+    def save_data(self):
+        self.data["config"]["main_url"] = self.domen_line.text()
+        self.data["data"]=[]
+        for i in self.blocks:
+            self.data["data"].append(i.getData())
+        print(self.data)
+        with open(self.file, "w", encoding="utf-8") as f:
+            json.dump(self.data, f, ensure_ascii=False, indent=2)
 
 
 app = QApplication(sys.argv)
